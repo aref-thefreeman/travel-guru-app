1bf3a83e8f6450e764fc472885136fc28c97a9e3
aref-thefreeman <mdarefbillah818@gmail.com>
2020-09-25 18:58:52 +0600

    version-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import {BrowserRouter as Router, Route } from "react-router-dom";
+import Header from './Components/Header/Header';
+import LogIn from './Components/Header/LogIn/LogIn';
+import About from './Components/Header/About/About'
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header></Header>
+        <switch>
+          <Route path = "/about">
+            <About></About>
+          </Route>
+
+          <Route path = "/login">
+            <LogIn></LogIn>
+          </Route>
+        </switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Header/LogIn/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogIn = () => {
+    return (
+        <div>
+            I am from log in
+        </div>
+    );
+};
+
+export default LogIn;
